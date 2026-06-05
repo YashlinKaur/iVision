ff81a174b947fe616c2ee3fc1d825aa6ccd73fda
YashlinKaur <yashlinkaurg@gmail.com>
2026-06-05 16:12:46 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './Home'
 import About from './Components/Aboutus/About'
 import MacAccessories from './Components/Accessories/MacAccessories'
 import Storemain from './Components/Store/Storemain/Storemain'
+import Latest from './Components/Store/Latest'
 import Login from "./Components/Login/Login";
 import Register from './Components/Register/Register'
 import Keyboards from './Components/Accessories/Mac/Keyboards'
@@ -26,9 +27,11 @@ const App = () => {
           <Route path="/" element={<Home />} />
            <Route path="/aboutus" element={<About />} />
            <Route path="/login" element={<Login />} />
-           <Route path="/mac" element={<MacAccessories />} />
-           <Route path="/Store" element={<Storemain/>} />
            <Route path="/Register" element={<Register />} />
+
+           <Route path="/Store" element={<Storemain/>} />
+           <Route path="/Latest" element={<Latest />} />
+           <Route path="/mac" element={<MacAccessories />} />
            <Route path="/keyboards" element={<Keyboards />} />
            <Route path="/mice" element={<Mice />} />
            <Route path="/hubs" element={<Hubs />} />
